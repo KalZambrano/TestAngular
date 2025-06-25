079d4df54846ebb6e904f58a54b6e013e71e8ce3
Kaleb <kalebzambrano@outlook.com>
2025-06-25 14:45:38 -0500

    exercise 2 complete

--- src/app/components/exercise2/exercise2.component.ts
+++ src/app/components/exercise2/exercise2.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './exercise2.component.html',
   styles: ``
 })
 export class Exercise2Component {
+  edad:number = 0;
+  grupoEtario:string = ''
 
+  designarGrupo(){
+    if (this.edad > 0 && this.edad < 12) {
+      this.grupoEtario = 'Niño';
+    }else if (this.edad >= 12 && this.edad <= 17){
+      this.grupoEtario = 'Adolescente'
+    }else if (this.edad >= 18 && this.edad <= 64){
+      this.grupoEtario = 'Adulto'
+    }else if(this.edad >= 65){
+      this.grupoEtario = 'Adulto Mayor'
+    }else{
+      this.grupoEtario = 'Edad no válida'
+    }
+  }
 }
